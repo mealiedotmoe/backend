219687b8f551f9de8f38abeae07ca310cdf214a9
dashwav <dashwav@gmail.com>
2018-06-13 01:16:26 -0700

    remove unused route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 
 const frontEnd = path.resolve('../Jpk/dist');
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 
 app.use('*', express.static(frontEnd));
