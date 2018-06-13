4fda76d22726f6ddcb57a2284a435dcd50885086
dashwav <dashwav@gmail.com>
2018-06-13 02:32:42 -0700

    using http until ssl is set up

--- routes/login.js
+++ routes/login.js
@@ -60,7 +60,7 @@ router.get('/callback', async function(req, res, next) {
   };
   var token = jwt.sign(claims, 'tokengoeshere');
   res.cookie('user', token, {httpOnly: true});
-  res.redirect('https://animeirl.xyz/dash');
+  res.redirect('http://animeirl.xyz/dash');
 });
 
 
