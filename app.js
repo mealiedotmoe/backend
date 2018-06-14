bc2fbd5008bb1b765983c92f22865adba70cb827
dashwav <dashwav@gmail.com>
2018-06-14 00:37:10 -0700

    changed nginx config

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/discord/login', loginRouter);
+app.use('/discord/login', loginRouter);
 
 
 // catch 404 and forward to error handler
