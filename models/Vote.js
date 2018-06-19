85470cf70ddfc03bf2503198ab9ce80d421ce6d0
dashwav <dashwav@gmail.com>
2018-06-19 11:13:28 -0700

    vote updating to module.exports

--- models/Vote.js
+++ models/Vote.js
@@ -1,12 +1,8 @@
-var Vote = sequelize.define('Vote', {
-    ip: DataTypes.STRING,
-    user: DataTypes.STRING,
-    // ... define model fields here
+module.exports = (sequelize, DataTypes) => sequelize.define('vote', {
   }, {
     classMethods: {
-      associate: function(models) {
-        // define relationships here
-      }
-        // ... define class methods here
+        associate: function(models) {
+            vote.hasOne(models.user);
+        }
     }
-  });
+});
