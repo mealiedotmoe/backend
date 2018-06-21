9dab5e417d99b9272f08ef070b49825032519657
dashwav <dashwav@gmail.com>
2018-06-21 11:50:08 -0700

    adding add poll func

--- routes/polls.js
+++ routes/polls.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+const jwtSecret = 'yourtokenhere';
 const btoa = require('btoa');
 const fetch = require('node-fetch');
 
@@ -11,11 +12,29 @@ const sequelize = new Sequelize('mealiedb', 'mealie', 'password', {
   logging: false,
   operatorsAliases: false,
 });
+
+const Users = sequelize.import('../models/User');
 const Questions = sequelize.import('../models/Question');
 const Choices = sequelize.import('../models/Choice');
 const Votes = sequelize.import('../models/Vote');
 
 const redirect = encodeURIComponent('https://www.animeirl.xyz/api/v1/discord/login/callback');
+
+function getUser(req) {
+    if (!req.headers.authorization) {
+        return false;
+    }
+    const token = req.headers.authorization.split(' ')[1];
+
+    jwt.verify(token, jwtSecret, (err, decoded) =>{
+        if(err) { return false; }
+        const userId = decoded.sub;
+        return Users.findById(userId).then(user => {
+            if (!user) { return false; }
+            return user;
+        }).catch(err => { return false; })
+    })
+}
 
 /* GET all questions */
 router.get('/', function(req, res, next) {
@@ -26,18 +45,22 @@ router.get('/', function(req, res, next) {
 
 /* POST to create new question */
 router.post('/', async function(req, res, next) {
+    user = getUser(req);
+    if (!user) { return res.status(401).send('You must be logged in to use this feature').end(); }
     Questions.create({
-        text: req.body.questionText,
-        multiple_options: req.body.multiple_options,
+        text: req.body.pollName,
+        multiple_options: req.body.enableMultipleOptions,
     }).then(newQuestion => {
-        req.body.newChoices.forEach((choice) => {
+        newQuestion.setUser(user);
+        req.body.choices.forEach((choice) => {
             Choices.create({
-                questionId: newQuestion.id,
                 text: choice.text,
+            }).then(choice => {
+                choice.setQuestion(newQuestion)
             });
         });
         res.send(newQuestion);
-    })
+    });
 });
 
 router.get('/polls/:id', function(req, res, next){
