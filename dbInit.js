49b1e9555feb63d3569b45b1c79a30265b27de7c
dashwav <dashwav@gmail.com>
2018-06-22 01:49:51 -0700

    Finish up DB stuff

--- dbInit.js
+++ dbInit.js
@@ -12,11 +12,6 @@ var Questions = sequelize.import('models/Question');
 var Choices = sequelize.import('models/Choice');
 var Votes = sequelize.import('models/Vote');
 
-Questions.belongsTo(Users, {as: 'User'});
-Questions.hasMany(Choices, {as: 'Choices'});
-Choices.hasMany(Votes, {as: 'Votes'});
-Votes.belongsTo(Users, {as: 'Users'})
-
 const force = process.argv.includes('--force') || process.argv.includes('-f');
 
 sequelize.sync({ force }).then(async () => {
