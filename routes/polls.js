1ac49f75fc3b5ac8782f545fb417b50c1e2246f8
dashwav <dashwav@gmail.com>
2018-06-23 01:49:43 -0700

    debugging

--- routes/polls.js
+++ routes/polls.js
@@ -46,9 +46,11 @@ async function getResponses(question) {
 /* GET all questions */
 router.get('/', async function(req, res, next) {
     Questions.findAndCountAll().then(questions => {
+        console.log(questions)
         questions['rows'] = questions['rows'].map(async question => {
             question.author = JSON.stringify(await question.getUser());
             question.responses = await getResponses(question);
+            console.log(question)
             return question;
         });
         res.send(JSON.stringify(questions));
