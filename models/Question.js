f294f6877ea12d11fbbfd25a2a5e491809987e0f
dashwav <dashwav@gmail.com>
2018-06-23 00:55:20 -0700

    oky

--- models/Question.js
+++ models/Question.js
@@ -1,4 +1,4 @@
-module.exports = (sequelize, DataTypes) =>{ 
+module.exports = (sequelize, DataTypes) => 
     sequelize.define('question', {
         text: {
         type: DataTypes.STRING,
@@ -16,4 +16,3 @@ module.exports = (sequelize, DataTypes) =>{
             defaultValue: false,
         }
     });
-};
