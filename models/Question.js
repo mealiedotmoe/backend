d22910ce2331fece9e6fa9c951b8f9b480ccb7d6
dashwav <dashwav@gmail.com>
2018-06-23 01:42:31 -0700

    giving up right now

--- models/Question.js
+++ models/Question.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) =>
         type: DataTypes.STRING,
         allowNull: false,
         },
-        author: {
+        UserDiscordId: {
             type: DataTypes.STRING,
         },
         multiple_options: {
