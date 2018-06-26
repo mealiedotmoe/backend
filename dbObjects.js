7a170b371e916326df0cae433ad18a3c2bfb742c
dashwav <dashwav@gmail.com>
2018-06-25 23:15:00 -0700

    Add new models

--- dbObjects.js
+++ dbObjects.js
@@ -12,9 +12,15 @@ var Questions = sequelize.import('models/Question');
 var Choices = sequelize.import('models/Choice');
 var Votes = sequelize.import('models/Vote');
 
+var FaqInfo = sequelize.import('models/FaqInfo');
+var Markdown = sequelize.import('models/Markdown');
+
 Questions.belongsTo(Users, {as: 'User', foreignKey: 'author'});
 Questions.hasMany(Choices, {as: 'Choices'});
 Choices.hasMany(Votes, {as: 'Votes'});
 Votes.belongsTo(Users, {as: 'User', foreignKey: 'userId'});
 
-module.exports = {Users, Questions, Choices, Votes}
+FaqInfo.belongsTo(Users, {as: 'User', foreignKey: 'author'});
+Markdown.belongsTo(Users, {as: 'Users', foreignKey: 'author'});
+
+module.exports = {Users, Questions, Choices, Votes, FaqInfo, Markdown}
---
+++ models/Markdown.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => 
+    sequelize.define('markdown', {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        slug: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+        },
+        last_edit: {
+            type: DataTypes.STRING,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
