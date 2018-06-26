31f654559a89def9dd62664ea9f8e04428050baa
dashwav <dashwav@gmail.com>
2018-06-26 00:54:04 -0700

    add faq router

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use('/users', userRouter);
 app.use('/polls', pollRouter);
 app.use('/discord/login', loginRouter);
 app.use('/info', infoRouter);
+app.use('/faq', faqRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
