a2101cf0af99efb96003245d4569951c45353d97
dashwav <dashwav@gmail.com>
2018-08-03 16:00:00 -0700

    user_id int-> str

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => sequelize.define('subscription', {
     game_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER,
+    user_id: DataTypes.STRING,
     created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 });
