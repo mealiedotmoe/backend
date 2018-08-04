bb20bb828ac0586548bde645101aa9a5a80da543
dashwav <dashwav@gmail.com>
2018-08-03 23:28:43 -0700

    adding frequency

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,5 +1,6 @@
 module.exports = (sequelize, DataTypes) => sequelize.define('subscription', {
     game_id: DataTypes.INTEGER,
     user_id: DataTypes.STRING,
+    frequency: DataTypes.STRING,
     created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 });
