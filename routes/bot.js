9acbae6bd84a1b62089accc4acc0cd466319167f
dashwav <dashwav@gmail.com>
2018-08-07 17:47:25 -0700

    Adding /colors/:number route

--- routes/bot.js
+++ routes/bot.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const snekfetch = require('snekfetch');
 const {BotToken} = require('../config');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const colorLevels = [
+    '-2-',
+    '-5-',
+    '-10-',
+    '-15-',
+    '-20-',
+    '-25-',
+    '-30-',
+];
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -36,6 +47,32 @@ router.post('/iam/', function(req, res, next) {
         const user = guild.members.get(req.user.discord_id);
         user.addRole(role, 'added from website');
         res.status(200).send("okayhand");
+    }
+    catch (err) {
+        res.status(500).end();
+    }
+});
+
+router.post('/colors/:number', async function(req, res, next) {
+    try {
+        const userLevel = await snekfetch.get(
+            `https://www.danbo.space/api/v1/servers/148606162810568704/user/${req.user.discord_id}`
+        );
+        if (userLevel.statusCode !== 200) { return res.status(500).send("internal error").end(); }
+        let userParse = JSON.parse(userLevel.text);
+        let intNumber = parseInt(req.params.number);
+        if (userParse['user']['level'] < intNumber || intNumber > 30) {
+            return res.status(500).send('User not high enough level').end();
+        }
+        const guild = client.guilds.get('148606162810568704');
+        const role = guild.roles.find('name', `-${intNumber}-`);
+        const user = guild.members.get(req.user.discord_id);
+        const removeRoles = await Promise.all(colorLevels.map(async level =>{
+            return guild.roles.find('name', level);
+        }));
+        await user.removeRoles(removeRoles, 'Removing old color roles');
+        await user.addRole(role, 'Adding new color role');
+        res.status(200).send("ok_hand");
     }
     catch (err) {
         res.status(500).end();
