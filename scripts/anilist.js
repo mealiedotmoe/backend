0976abf45cd4276f924a049db6f5f00e8b962556
dashwav <dashwav@gmail.com>
2019-03-13 23:41:38 -0700

    this will look nicer

--- scripts/anilist.js
+++ scripts/anilist.js
@@ -3,13 +3,17 @@ const Sequelize = require('sequelize');
 
 const Op = Sequelize.Op;
 
-var allUsers = Users.all({
+var allUsers = await Users.all({
   where: {
     anilist: {
       [Op.ne]: null
     }
   }
 });
+
+if (!allUsers) {
+  return;
+}
 
 allUsers.forEach(user => {
   console.log(user);
