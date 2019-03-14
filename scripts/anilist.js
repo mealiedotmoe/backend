778e26e4b146132465b8f5f3b7f6f18cb5439296
dashwav <dashwav@gmail.com>
2019-03-14 00:11:51 -0700

    es6 again :pensive2sdfs

--- scripts/anilist.js
+++ scripts/anilist.js
@@ -1,6 +1,7 @@
 const {Users} = require('../dbObjects');
 const {GetUserList} = require('./queries/GetUserList');
 const Sequelize = require('sequelize');
+const fetch = require("fetch");
 
 const Op = Sequelize.Op;
 
