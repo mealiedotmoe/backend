a9fbdb08824a8a5257e16448cca49c2e3458ea28
dashwav <dashwav@gmail.com>
2019-03-13 23:29:01 -0700

    Add scripts

---
+++ scripts/anilist.js
@@ -0,0 +1,11 @@
+const {Users} = require('../dbObjects');
+
+var allUsers = Users.all({
+  where: {
+    anilist: {
+      [Op.ne]: null
+    }
+  }
+});
+
+console.log(allUsers)
---
+++ scripts/queries/GetUserList.js
@@ -0,0 +1,35 @@
+export const
+  UserList = `
+  query ($userName: String!) {
+    User(id: $userName) {
+      id
+      name
+      avatar {
+        medium
+      }
+    }
+    MediaListCollection(userName: $userName, type:ANIME) {
+      lists {
+        name
+        entries {
+          id
+          progress
+          status
+          media {
+            id
+            episodes
+            title {
+              userPreferred
+            }
+            coverImage{
+              medium
+            }
+            genres
+          }
+          mediaId
+          score
+        }
+      }
+    }
+  }
+  `
